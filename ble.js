a5229f78967a3d4f3ee99392be550f0990a8e40a
John Rucker <JHRucker@outlook.com>
2020-05-04 11:26:47 -0500

    .

--- ble.js
+++ ble.js
@@ -19,4 +19,4 @@ class blePeripheral{
     }
 }
 
-module.exports = blePeripheral;
+export default blePeripheral;
--- devTest.js
+++ devTest.js
@@ -1,11 +1,11 @@
 // const blePeripheral =   require("./blePeripheral.js");
-const blePeripheral =       require("./ble.js")
+const blePeripheral =       require("./ble.js").default
 
 const serviceName = 'com.sampleApp';                          // peripheral's DBus service name
 const serviceUUID = '27b5244f-94f3-4011-be53-6ac36bf22cf1'    // UUID to advertise as an Bluetooh LE service
 
 
-const bPrl = new blePeripheral(main.bind(this));
+const bPrl = new blePeripheral(main);
     
 function main(){
     console.log('Main call back is starting now..');
