173da064ef75963a7699d0e6cd261f46e0d158ee
John Rucker <JHRucker@outlook.com>
2020-05-04 09:14:40 -0500

    .

--- ble.js
+++ ble.js
@@ -4,12 +4,8 @@ const EventEmitter =      require("events");
 class blePeripheral extends EventEmitter{
     /**
      * This is a test class to understand why my call back is not working.
-     * @param {*} ServiceName 
-     * @param {*} ServerUUID 
-     * @param {*} callback 
-     * @param {*} PrimaryService 
      */
-    constructor(ServiceName ='com.netConfig', ServerUUID = '4b1268a8-d692-41d6-b51a-d1730ea6b9d6', callback = function(){}, PrimaryService = true){
+    constructor(callback){
         super()
         this.logAllDBusMessages = false;
         this.logCharacteristicsIO = false;
--- devTest.js
+++ devTest.js
@@ -5,7 +5,7 @@ const serviceName = 'com.sampleApp';                          // peripheral's DB
 const serviceUUID = '27b5244f-94f3-4011-be53-6ac36bf22cf1'    // UUID to advertise as an Bluetooh LE service
 
 
-const bPrl = new blePeripheral(serviceName, serviceUUID, main);
+const bPrl = new blePeripheral(main);
     
 function main (){
     bPrl.logCharacteristicsIO = true;
