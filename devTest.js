ca8796777a4739e5af773aef82d0d5ff79283406
John Rucker <JHRucker@outlook.com>
2020-05-04 09:28:20 -0500

    .

--- devTest.js
+++ devTest.js
@@ -5,7 +5,9 @@ const serviceName = 'com.sampleApp';                          // peripheral's DB
 const serviceUUID = '27b5244f-94f3-4011-be53-6ac36bf22cf1'    // UUID to advertise as an Bluetooh LE service
 
 
-var bPrl = new blePeripheral(()=>{
+var bPrl = new blePeripheral(main);
+    
+function main (){
     console.log('Main call back is starting now..');
     bPrl.logCharacteristicsIO = true;
     console.log('logCharacteristicsIO enabled = ' + bPrl.logCharacteristicsIO);
@@ -15,4 +17,4 @@ var bPrl = new blePeripheral(()=>{
 
     //var isAuthorized =  bp.Characteristic('00000001-94f3-4011-be53-6ac36bf22cf1', 'isAuthorized', ["read","write-without-response"]);
 
-});
+};
