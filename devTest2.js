c075356e319363da81c01e0e54f29732fc3f941d
John Rucker <JHRucker@outlook.com>
2020-05-12 10:02:54 -0500

    .

--- devTest2.js
+++ devTest2.js
@@ -29,140 +29,154 @@ var obj = service.createObject('/nodejs/dbus/ExampleService');
 
 var iface1 = obj.createInterface('com.sampleApp.org.bluez.GattService1');
 
-iface1.addMethod('Dummy', {}, function(callback) {
-	setTimeout(function() {
-		callback();
-	}, 1000);
-});
-
-iface1.addMethod('MakeError', { out: DBus.Define(String) }, function(callback) {
-	callback(new DBus.Error('nodejs.dbus.ExampleService.ErrorTest', 'Some error'));
-});
-
-iface1.addMethod('Hello', { out: DBus.Define(String) }, function(callback) {
-	callback(null, 'Hello There!');
-});
-
-iface1.addMethod('SendObject', { in: [ DBus.Define(Object) ], out: DBus.Define(Object) }, function(obj, callback) {
-	callback(null, obj);
-});
-
-iface1.addMethod('SendVarient', { in: [ DBus.Define('Auto') ], out: DBus.Define('Auto') }, function(obj, callback) {
-	console.log(obj);
-	callback(null, obj);
-});
-
-iface1.addMethod('Ping', { out: DBus.Define(String) }, function(callback) {
-	callback('Pong!');
-});
-
-iface1.addMethod('Equal', {
-	in: [
-		DBus.Define(Number),
-		DBus.Define(Number)
-	],
-	out: DBus.Define(Boolean)
-}, function(a, b, callback) {
-
-	if (a == b)
-		callback(null, true);
-	else
-		callback(null, false);
-
-});
-
-iface1.addMethod('GetNameList', { out: DBus.Define(Array, 'list') }, function(callback) {
-	callback(null, [
-		'Fred',
-		'Stacy',
-		'Charles',
-		'Rance',
-		'Wesley',
-		'Frankie'
-	]);
-});
-
-iface1.addMethod('GetContacts', { out: DBus.Define(Object, 'contacts') }, function(callback) {
-	callback(null, {
-		Fred: {
-			email: 'fred@mandice.com',
-			url: 'http://fred-zone.blogspot.com/',
-			age: 28,
-			tel: [
-				'09263335xx',
-				'0936123456'
-			]
-		},
-		Stacy: {
-			email: 'stacy@mandice.com',
-			age: 28,
-			url: 'http://www.mandice.com/'
-		},
-		Charles: {
-			email: 'charles@mandice.com',
-			url: 'http://www.mandice.com/'
-		},
-		Rance: {
-			email: 'lzy@mandice.com',
-			url: 'http://www.mandice.com/'
-		},
-		Wesley: {
-			email: 'wesley@mandice.org',
-			url: 'http://www.mandice.org/'
-		},
-		Frankie: {
-			email: 'frankie@mandice.com',
-			address: [
-				{
-					country: 'Taipei'
-				},
-				{
-					country: 'New Taipei'
-				}
-			]
-		}
-	});
-});
-
-// Writable property
-var author = 'Fred Chien';
-iface1.addProperty('Author', {
-	type: DBus.Define(String),
-	getter: function(callback) {
-		callback(null, author);
-	},
-	setter: function(value, complete) {
-		author = value;
-
-		complete();
+iface1.addProperty('Primary', {
+	type: {type: 'b'},
+	getter: (callback)=>{
+		callback(null, this.prmary);
 	}
 });
 
-// Read-only property
-var url = 'http://stem.mandice.org';
-iface1.addProperty('URL', {
-	type: DBus.Define(String),
-	getter: function(callback) {
-		callback(null, url);
-	}
-});
+iface1.addProperty('UUID', {
+type: {type: 's'},
+getter: (callback)=>{
+	callback(null, this.UUID);
+}
+});  
 
-// Read-only property
-var jsOS = 'Stem OS';
-iface1.addProperty('JavaScriptOS', {
-	type: DBus.Define(String),
-	getter: function(callback) {
-		callback(null, jsOS);
-	}
-});
+// iface1.addMethod('Dummy', {}, function(callback) {
+// 	setTimeout(function() {
+// 		callback();
+// 	}, 1000);
+// });
 
-// Signal
-var counter = 0;
-iface1.addSignal('pump', {
-	types: [
-		DBus.Define(Number)
-	]
-});
+// iface1.addMethod('MakeError', { out: DBus.Define(String) }, function(callback) {
+// 	callback(new DBus.Error('nodejs.dbus.ExampleService.ErrorTest', 'Some error'));
+// });
+
+// iface1.addMethod('Hello', { out: DBus.Define(String) }, function(callback) {
+// 	callback(null, 'Hello There!');
+// });
+
+// iface1.addMethod('SendObject', { in: [ DBus.Define(Object) ], out: DBus.Define(Object) }, function(obj, callback) {
+// 	callback(null, obj);
+// });
+
+// iface1.addMethod('SendVarient', { in: [ DBus.Define('Auto') ], out: DBus.Define('Auto') }, function(obj, callback) {
+// 	console.log(obj);
+// 	callback(null, obj);
+// });
+
+// iface1.addMethod('Ping', { out: DBus.Define(String) }, function(callback) {
+// 	callback('Pong!');
+// });
+
+// iface1.addMethod('Equal', {
+// 	in: [
+// 		DBus.Define(Number),
+// 		DBus.Define(Number)
+// 	],
+// 	out: DBus.Define(Boolean)
+// }, function(a, b, callback) {
+
+// 	if (a == b)
+// 		callback(null, true);
+// 	else
+// 		callback(null, false);
+
+// });
+
+// iface1.addMethod('GetNameList', { out: DBus.Define(Array, 'list') }, function(callback) {
+// 	callback(null, [
+// 		'Fred',
+// 		'Stacy',
+// 		'Charles',
+// 		'Rance',
+// 		'Wesley',
+// 		'Frankie'
+// 	]);
+// });
+
+// iface1.addMethod('GetContacts', { out: DBus.Define(Object, 'contacts') }, function(callback) {
+// 	callback(null, {
+// 		Fred: {
+// 			email: 'fred@mandice.com',
+// 			url: 'http://fred-zone.blogspot.com/',
+// 			age: 28,
+// 			tel: [
+// 				'09263335xx',
+// 				'0936123456'
+// 			]
+// 		},
+// 		Stacy: {
+// 			email: 'stacy@mandice.com',
+// 			age: 28,
+// 			url: 'http://www.mandice.com/'
+// 		},
+// 		Charles: {
+// 			email: 'charles@mandice.com',
+// 			url: 'http://www.mandice.com/'
+// 		},
+// 		Rance: {
+// 			email: 'lzy@mandice.com',
+// 			url: 'http://www.mandice.com/'
+// 		},
+// 		Wesley: {
+// 			email: 'wesley@mandice.org',
+// 			url: 'http://www.mandice.org/'
+// 		},
+// 		Frankie: {
+// 			email: 'frankie@mandice.com',
+// 			address: [
+// 				{
+// 					country: 'Taipei'
+// 				},
+// 				{
+// 					country: 'New Taipei'
+// 				}
+// 			]
+// 		}
+// 	});
+// });
+
+// // Writable property
+// var author = 'Fred Chien';
+// iface1.addProperty('Author', {
+// 	type: DBus.Define(String),
+// 	getter: function(callback) {
+// 		callback(null, author);
+// 	},
+// 	setter: function(value, complete) {
+// 		author = value;
+
+// 		complete();
+// 	}
+// });
+
+// // Read-only property
+// var url = 'http://stem.mandice.org';
+// iface1.addProperty('URL', {
+// 	type: DBus.Define(String),
+// 	getter: function(callback) {
+// 		callback(null, url);
+// 	}
+// });
+
+// // Read-only property
+// var jsOS = 'Stem OS';
+// iface1.addProperty('JavaScriptOS', {
+// 	type: DBus.Define(String),
+// 	getter: function(callback) {
+// 		callback(null, jsOS);
+// 	}
+// });
+
+// // Signal
+// var counter = 0;
+// iface1.addSignal('pump', {
+// 	types: [
+// 		DBus.Define(Number)
+// 	]
+// });
 
 iface1.update();
 
