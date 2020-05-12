8126ede6bfd1aefa4a3d153e4c539e07980cae0b
John Rucker <JHRucker@outlook.com>
2020-05-12 06:35:55 -0500

    .

--- devTest.js
+++ devTest.js
@@ -1,4 +1,6 @@
 const blePeripheral =   require("./blePeripheral.js");
+// "dbus": "git+https://github.com/RuckerGauge/node-dbus.git#JR_addSupportForObjectPath"
+// "dbus": "^1.0.7"
 
 const serviceName = 'com.sampleApp';                          // peripheral's DBus service name
 const serviceUUID = '27b5244f-94f3-4011-be53-6ac36bf22cf1'    // UUID to advertise as an Bluetooh LE service
