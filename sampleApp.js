e198bbfd2c730621b82f9f63eb0a0e32274935ed
John Rucker <JHRucker@outlook.com>
2020-05-14 15:54:20 -0500

    .

--- sampleApp.js
+++ sampleApp.js
@@ -77,7 +77,7 @@ function main(DBus){
 
       case '6':
         console.log('Testing setting trusted to false');
-        bPrl.Device.setBooleanProperty('rusted', false);
+        bPrl.Device.setBooleanProperty('Trusted', 'false');
       break;
     
       default:
